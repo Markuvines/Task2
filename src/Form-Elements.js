69fbe9d59727249912823c244f8da25d5d9d0d28
Markuvines <shinigami_master@mail.ru>
2020-07-14 17:56:51 +0300

    Connected 'checkbox-list.js'

--- src/Form-Elements.js
+++ src/Form-Elements.js
@@ -1,2 +1,3 @@
 import './blocks/Form-Elements/dopdown-guests/dopdown-guests';
 import './blocks/Form-Elements/dropdown-room/dropdown-room';
+import './blocks/Form-Elements/checkbox-list/checkbox-list';
