c7620ba27641b0aafd86637d7d9cad32da5d1ab5
Markuvines <shinigami_master@mail.ru>
2020-07-17 15:09:48 +0300

    Connected 'toggle.js' to 'Form-Elements.js'

--- src/Form-Elements.js
+++ src/Form-Elements.js
@@ -1,3 +1,4 @@
 import './blocks/Form-Elements/dopdown-guests/dopdown-guests';
 import './blocks/Form-Elements/dropdown-room/dropdown-room';
 import './blocks/Form-Elements/checkbox-list/checkbox-list';
+import './blocks/Form-Elements/toggle/toggle'
