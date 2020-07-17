d6231ca7182c28ea6121308e55cb1f2c01c2fff1
Markuvines <shinigami_master@mail.ru>
2020-07-17 21:44:58 +0300

    Connected 'like-button.js'

--- src/Form-Elements.js
+++ src/Form-Elements.js
@@ -1,4 +1,5 @@
 import './blocks/Form-Elements/dopdown-guests/dopdown-guests';
 import './blocks/Form-Elements/dropdown-room/dropdown-room';
 import './blocks/Form-Elements/checkbox-list/checkbox-list';
-import './blocks/Form-Elements/toggle/toggle'
+import './blocks/Form-Elements/toggle/toggle';
+import './blocks/Form-Elements/like-button/like-button';
