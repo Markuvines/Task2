5fbe2d5bcd007d0fefeae69e4384f79e98bfeb00
Markuvines <shinigami_master@mail.ru>
2020-07-20 19:19:14 +0300

    Reworked 'dropdown-room.js'

--- src/blocks/Form-Elements/dropdown-room/dropdown-room.js
+++ src/blocks/Form-Elements/dropdown-room/dropdown-room.js
@@ -5,10 +5,9 @@ $( document ).ready(function() {
     ++count;
     $(this).siblings('.minus-button').addClass('minus-button--fill')
     $(this).siblings('.dropdown-room__count').text(count);
-    var $baths = $('.baths').children('.dropdown-room__count').text();
-    var $beds = $('.beds').children('.dropdown-room__count').text();
-    var $bedrooms = $('.bedrooms').children('.dropdown-room__count').text();
-    $('.dropdown-room__text-field-default').val($bedrooms + ' cпальни, ' + $beds + ' кровати...')
+    var $beds = $(this).parents('.dropdown-room__list').find('.beds').children('.dropdown-room__count').text();
+    var $bedrooms = $(this).parents('.dropdown-room__list').find('.bedrooms').children('.dropdown-room__count').text();
+    $(this).parents('.dropdown-room__dropdown-body').siblings('.dropdown-room__dropdown-field').children('.dropdown-room__text-field-default').val($bedrooms + ' cпальни, ' + $beds + ' кровати...')
   });
   $('.minus-button').on('click',function(){
     var count = $(this).siblings('.dropdown-room__count').text();
@@ -19,14 +18,14 @@ $( document ).ready(function() {
       $(this).siblings('.dropdown-room__count').text(count);}
     else
       $(this).siblings('.dropdown-room__count').text(count);
-    var $beds = $('.beds').children('.dropdown-room__count').text();
-    var $bedrooms = $('.bedrooms').children('.dropdown-room__count').text();
-    $('.dropdown-room__text-field-default').val($bedrooms + ' cпальни, ' + $beds + ' кровати...')
+      var $beds = $(this).parents('.dropdown-room__list').find('.beds').children('.dropdown-room__count').text();
+      var $bedrooms = $(this).parents('.dropdown-room__list').find('.bedrooms').children('.dropdown-room__count').text();
+      $(this).parents('.dropdown-room__dropdown-body').siblings('.dropdown-room__dropdown-field').children('.dropdown-room__text-field-default').val($bedrooms + ' cпальни, ' + $beds + ' кровати...')
   });
   $('.dropdown-room__arrow').on('click', function(){
-    $('.dropdown-room__dropdown-body').toggle();
+    $(this).parent().siblings('.dropdown-room__dropdown-body').toggle()
   });
   $('.dropdown-room__arrow').on('click', function(){
-    $('.dropdown-room__text-field-default').toggleClass('dropdown-room__text-field-default--active');
+    $(this).siblings('.dropdown-room__text-field-default').toggleClass('dropdown-room__text-field-default--active');
   });
 });
