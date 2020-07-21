7f8b93322ac4a6d80ff26e10fe851c17899dcbfa
Markuvines <shinigami_master@mail.ru>
2020-07-21 19:04:32 +0300

    Added 'dropdown-room', 'dropdown-guests', 'minus-button', 'plus-button' mixins

--- src/Form-Elements.js
+++ src/Form-Elements.js
@@ -1,6 +1,53 @@
-import './blocks/Form-Elements/dopdown-guests/dopdown-guests';
+import './blocks/Form-Elements/dopdown-guests/dropdown-guests';
 import './blocks/Form-Elements/dropdown-room/dropdown-room';
 import './blocks/Form-Elements/checkbox-list/checkbox-list';
 import './blocks/Form-Elements/toggle/toggle';
 import './blocks/Form-Elements/like-button/like-button';
 import './blocks/Form-Elements/star-rate/star-rate';
+// $(".range-example-1").asRange({
+
+//   // namespace
+//   namespace: 'asRange',
+
+//   // requires additional skin file
+//   skin: null,
+
+//   // max value
+//   max: 20000,
+
+//   // min value
+//   min: 0,
+
+//   // initial value
+//   value: 5000,
+
+//   // moving step at a time
+//   step: 10,
+
+//   // limit the range of the pointer moving
+//   limit: true,
+
+//   // initial range
+//   range: true,
+
+//   // 'v' or 'h'
+//   direction: 'h', 
+
+//   // enable keyboard interactions
+//   keyboard: true,
+
+//   // false, 'inherit', {'inherit': 'default'}
+//   replaceFirst: false, 
+
+//   // shows tips
+//   tip: false,
+
+//   // shows scales
+//   scale: true,
+
+//   // for formatting output
+//   format: function format(value) {
+//     return value;
+//   }
+
+// });
---
+++ src/blocks/Form-Elements/dopdown-guests/dropdown-guests.js
@@ -0,0 +1,45 @@
+$( document ).ready(function() {
+  var guests = 0;
+  $('.plus-button').on('click',function(){
+    var count = $(this).siblings('.dropdown-guests__count').text();
+    ++count;
+    $(this).siblings('.dropdown-guests__count').text(count);
+    ++guests
+    $('.dropdown-guests__reset').show()
+    if (guests==1||guests==21) 
+      $('.dropdown-guests__text-field-default').val(guests + ' гость');
+    else  if (guests==2||guests==3||guests==4||guests==22||guests==23||guests==24)
+            $('.dropdown-guests__text-field-default').val(guests + ' гостя');
+          else
+            $('.dropdown-guests__text-field-default').val(guests + ' гостей');
+  });
+  $('.minus-button').on('click',function(){
+    var count = $(this).siblings('.dropdown-guests__count').text();
+    --count;
+    if (count<0)
+      count = 0;
+    else
+    --guests;
+    $(this).siblings('.dropdown-guests__count').text(count);
+    if (guests == 0)
+      $('.dropdown-guests__reset').hide();
+    if (guests==1||guests==21) 
+      $('.dropdown-guests__text-field-default').val(guests + ' гость');
+    else  if (guests==2||guests==3||guests==4||guests==22||guests==23||guests==24)
+            $('.dropdown-guests__text-field-default').val(guests + ' гостя');
+          else
+            $('.dropdown-guests__text-field-default').val(guests + ' гостей');
+  });
+  $('.dropdown-guests').on('reset',function(){
+    count = 0;
+    guests = 0;
+    $('.dropdown-guests__count').text(count);
+    $('.dropdown-guests__reset').hide();
+  });
+  $('.dropdown-guests__arrow').on('click', function(){
+    $('.dropdown-guests__dropdown-body').toggle();
+  });
+  $('.dropdown-guests__arrow').on('click', function(){
+    $('.dropdown-guests__text-field-default').toggleClass('dropdown-guests__text-field-default--active');
+  });
+});
