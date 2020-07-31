66a3575309bd1359b0ea9e3546beaa8a8e388e02
Markuvines <shinigami_master@mail.ru>
2020-07-31 15:00:37 +0300

    Reworked 'dropdown-guests' and 'like-button' blocks

--- src/blocks/Form-Elements/dopdown-guests/dropdown-guests.js
+++ src/blocks/Form-Elements/dopdown-guests/dropdown-guests.js
@@ -4,17 +4,17 @@ $( document ).ready(function() {
     var count = $(this).siblings('.dropdown-guests__count').text();
     ++count;
     $(this).siblings('.dropdown-guests__count').text(count);
-    var $adults=+$(this).parents('.dropdown-guests__dropdown-body').find('[data-adults]').text()
-    var $children=+$(this).parents('.dropdown-guests__dropdown-body').find('[data-children]').text()
-    var $child = +$(this).parents('.dropdown-guests__dropdown-body').find('[data-child]').text()
-    var guests = $adults + $children + $child;
+    // var $adults=+$(this).parents('.dropdown-guests__dropdown-body').find('[data-adults]').text()
+    // var $children=+$(this).parents('.dropdown-guests__dropdown-body').find('[data-children]').text()
+    // var $child = +$(this).parents('.dropdown-guests__dropdown-body').find('[data-child]').text()
+    // var guests = $adults + $children + $child;
     $(this).parents('.dropdown-guests__dropdown-body').find('.dropdown-guests__reset').show()
-    if (guests==1||guests==21) 
-      $(this).parents('.dropdown-guests').find('.dropdown-guests__text-field-default').val(guests + ' гость');
-    else  if (guests==2||guests==3||guests==4||guests==22||guests==23||guests==24)
-      $(this).parents('.dropdown-guests').find('.dropdown-guests__text-field-default').val(guests + ' гостя');
-          else
-            $(this).parents('.dropdown-guests').find('.dropdown-guests__text-field-default').val(guests + ' гостей');
+    // if (guests==1||guests==21) 
+    //   $(this).parents('.dropdown-guests').find('.dropdown-guests__text-field-default').val(guests + ' гость');
+    // else  if (guests==2||guests==3||guests==4||guests==22||guests==23||guests==24)
+    //   $(this).parents('.dropdown-guests').find('.dropdown-guests__text-field-default').val(guests + ' гостя');
+    //       else
+    //         $(this).parents('.dropdown-guests').find('.dropdown-guests__text-field-default').val(guests + ' гостей');
   });
   $('.dropdown-guests__minus-button').on('click',function(){
     var count = +$(this).siblings('.dropdown-guests__count').text();
@@ -22,23 +22,36 @@ $( document ).ready(function() {
     if (count<0)
       count = 0;
     $(this).siblings('.dropdown-guests__count').text(count);
+    // var $adults=+$(this).parents('.dropdown-guests__dropdown-body').find('[data-adults]').text()
+    // var $children=+$(this).parents('.dropdown-guests__dropdown-body').find('[data-children]').text()
+    // var $child = +$(this).parents('.dropdown-guests__dropdown-body').find('[data-child]').text()
+    // var guests = $adults + $children + $child;
+    // if (guests<0)
+    //   guests = 0
+    if (count == 0)
+      $(this).removeClass('minus-button--fill');
+    // if (guests == 0)
+    //   $(this).parents('.dropdown-guests__dropdown-body').find('.dropdown-guests__reset').hide();
+    // if (guests==1||guests==21) 
+    //   $(this).parents('.dropdown-guests').find('.dropdown-guests__text-field-default').val(guests + ' гость');
+    // else  if (guests==2||guests==3||guests==4||guests==22||guests==23||guests==24)
+    //   $(this).parents('.dropdown-guests').find('.dropdown-guests__text-field-default').val(guests + ' гостя');
+    //       else
+    //         $(this).parents('.dropdown-guests').find('.dropdown-guests__text-field-default').val(guests + ' гостей');
+  });
+  $('.dropdown-guests__submit').on('click', function() {
     var $adults=+$(this).parents('.dropdown-guests__dropdown-body').find('[data-adults]').text()
     var $children=+$(this).parents('.dropdown-guests__dropdown-body').find('[data-children]').text()
     var $child = +$(this).parents('.dropdown-guests__dropdown-body').find('[data-child]').text()
     var guests = $adults + $children + $child;
-    if (guests<0)
-      guests = 0
-    if (count == 0)
-      $(this).removeClass('minus-button--fill');
-    if (guests == 0)
-      $(this).parents('.dropdown-guests__dropdown-body').find('.dropdown-guests__reset').hide();
+    // $(this).parents('.dropdown-guests__dropdown-body').find('.dropdown-guests__reset').show()
     if (guests==1||guests==21) 
       $(this).parents('.dropdown-guests').find('.dropdown-guests__text-field-default').val(guests + ' гость');
     else  if (guests==2||guests==3||guests==4||guests==22||guests==23||guests==24)
       $(this).parents('.dropdown-guests').find('.dropdown-guests__text-field-default').val(guests + ' гостя');
           else
             $(this).parents('.dropdown-guests').find('.dropdown-guests__text-field-default').val(guests + ' гостей');
-  });
+  })
   $('.dropdown-guests').on('reset',function(){
     count = 0;
     guests = 0;
--- src/blocks/Form-Elements/like-button/like-button.js
+++ src/blocks/Form-Elements/like-button/like-button.js
@@ -4,14 +4,12 @@ $('.like-button').on('click',function(){
   $count = $(this).children('.like-button__count').text()
   if ($(this).attr('data-status') == 'active') {
     $(this).toggleClass('like-button--active');
-    $(this).children('.like-button__like').children('img').attr('src', './src/img/favorite_border.svg')
     $count--
     $(this).children('.like-button__count').text($count)
     $(this).attr('data-status', 'disabled')
   }
   else {
     $(this).toggleClass('like-button--active');
-    $(this).children('.like-button__like').children('img').attr('src', './src/img/favorite.svg')
     $count++
     $(this).children('.like-button__count').text($count)
     $(this).attr('data-status', 'active')
